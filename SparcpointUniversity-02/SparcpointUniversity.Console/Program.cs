389358a0cd664c02f0579d0db5691f96d00d4dbc
Justin Coulston <justin.coulston@gmail.com>
2021-08-13 21:23:34 -0400

    Removed old unnecessary projects Reorganized Solutions Added ISqlExecutor Starter

---
+++ SparcpointUniversity-02/SparcpointUniversity.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SparcpointUniversity.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Insert Products
+
+            // Query All Products
+        }
+    }
+}
---
+++ SparcpointUniversity-02/SparcpointUniversity.Database/dbo/Tables/ProductAttributes.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ProductAttributes]
+(
+	[ProductId] INT NOT NULL,
+	[Key] VARCHAR(64) NOT NULL,
+	[Value] VARCHAR(MAX) NULL, 
+
+    CONSTRAINT [PK_ProductAttributes] PRIMARY KEY ([ProductId], [Key]), 
+    CONSTRAINT [FK_ProductAttributes_Products] FOREIGN KEY ([ProductId]) REFERENCES [dbo].[Products]([ProductId])
+)
